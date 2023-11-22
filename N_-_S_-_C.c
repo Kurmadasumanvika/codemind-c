1afabf15f7f126de341f8e520aa3441e86e3157b
kurmadasumanvika <147487907+Kurmadasumanvika@users.noreply.github.com>
2023-11-22 14:56:05 +0530

    #include<stdio.h> int main() {     int n,a,b;     scanf("%d %d",&a,&b);     n=n>a;     while(n<b)     {        if(n>a && n<b)        {            printf("%d %d %d ",n,n*n,n*n*n);        }        n++;     } }

---
+++ N_-_S_-_C.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int n,a,b;
+    scanf("%d %d",&a,&b);
+    n=n>a;
+    while(n<b)
+    {
+       if(n>a && n<b)
+       {
+           printf("%d %d %d
+",n,n*n,n*n*n);
+       }
+       n++;
+    }
+}
